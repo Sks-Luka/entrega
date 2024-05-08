e0910da5651b5a4c485cf05be2132ffda461996a
lucas pereyra <nicolaspereyradasilva@outlook.es>
2024-05-07 21:28:59 -0300

    Primera pre-entrega

--- app.js
+++ app.js
@@ -1,40 +1,25 @@
 import express from "express";
-import ProductManager from "./managerProducts/manager.js";
-
-const productManager = new ProductManager('../users.json');
+import productRouter from "./routes/product.router.js";
+import cartRouter from "./routes/cart.router.js";
+import { __dirname } from "./path.js";
+import {errorHandler} from "./middlewares/errorHandler.js";
+import morgan from "morgan";
 
 const app  = express();
 
-app.use(express.json())
+app.use (express.static(__dirname + 'public'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
 
-app.get('/products', async (req, res) => {
-    try {
-        const products = await productManager.getProducts();
-        const { limit } = req.query;
+app.use('/api/carts', cartRouter); 
+app.use('/api/products', productRouter);
 
-        if (limit){
-            const limitedList = products.slice(0, limit);
-            return res.status(200).json(limitedList);
-        }
-
-        return res.status(200).json(products);
-    } catch (error) {
-        res.status(500).json({ msg: error.message });
-    }
-})
-
-app.get("/products/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-        const product = await productManager.getProductById(id);
-        if (!product) res.status(404).json({ msg: "User not found" });
-        else res.status(200).json(product);
-    } catch (error) {
-        res.status(500).json({ msg: error.message });
-    }
-    });
+app.use(errorHandler);
 
 const PORT = 8080;
 
-app.listen(PORT,()=>console.log(`Servidor listo. Escuchando en el puerto ${PORT}`));
+app.listen(PORT,()=>{
+    console.log(`Servidor listo. Escuchando en el puerto ${PORT}`) 
+});
--- managerProducts/manager.js
+++ managerProducts/manager.js
@@ -1,132 +1,90 @@
 import  fs  from 'fs';
+import { v4 as uuidv4 } from "uuid";
+
 
 export default class ProductManager {
     constructor(path){
-        this.path = path
+        this.path = path;
     }
 
     async getProducts() {
         try {
-            if (fs.existsSync(this.path)) {
-                const product = await fs.promises.readFile(this.path, "utf8");//Leemos el path en caso de exitir
-                return JSON.parse(product)//Lo pasamos a formato JavaScript para poder manipularlo
-            } else return   [];
+            if (fs.existsSync(this.path)) {                                                                                              
+                const product = await fs.promises.readFile(this.path, "utf8");
+                return JSON.parse(product)
+        } else return   [];
+        } catch (error) {
+        console.log(error);
+        }
+    }
+
+
+    async addProducts(obj) {
+        try {
+            const product ={
+                id: uuidv4(),
+                status: true,
+                ...obj,
+            };
+            const products = await this.getProducts();
+            products.push(product); 
+            await fs.promises.writeFile(this.path, JSON.stringify(products));
+            return product;
             } catch (error) {
                 console.log(error);
             }
         }
+    
+    
 
-        async addProducts(product) {
+    async getProductById(id) {
+        try {
+            const products = await this.getProducts();
+            const productExist = products.find((p) => p.id === id);
+            if (!productExist) return null;
+            return productExist;
+        } catch (error) {
+            console.log(error);
+            }
+    }
+
+
+
+    async updateProduct(obj, id) {
+        try {
+            const products = await this.getProducts();
+            let productExist = await this.getProductById(id);
+            if (!productExist) return null;
+            productExist = { ...productExist, ...obj };
+            const newArray = products.filter((u) => u. id!== id);
+            newArray.push(productExist)
+            await fs.promises.writeFile(this.path, JSON.stringify(newArray));
+            return productExist;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
+    async deleteProduct(id) {
+        const products = await this.getProducts();
+        if (products.length > 0) {
+            const productExist = await this.getProductById(id);
+            if (productExist) {
+                const newArray = products.filter((u) => u.id !== id);
+                await fs.promises.writeFile(this.path, JSON.stringify(newArray));
+                return productExist
+                } 
+            } else return null
+        }
+
+
+        async deleteFile() {
             try {
-                const products = await this.getProducts(); // Obtengo los productos
-                product.id = this.getProductoID() + 1; // Le añadimos el id autogenerado
-        
-                // Verificamos si products es un array antes de usar push
-                if (!Array.isArray(products)) {
-                    throw new Error("La variable 'products' no es un array.");
-                }
-        
-                products.push(product); // Agregamos el producto al array
-                await fs.promises.writeFile(this.path, JSON.stringify(products)); // Guardo luego de pasarlo a JSON
+                await fs.promises.unlink(this.path);
+                console.log("archivo eliminado");
             } catch (error) {
                 console.log(error);
             }
         }
-        
-
-
-    async getProductById(id){
-        try {
-            const product = await this.getProducts();// Obtengo los productos
-            return product.find(product => product.id === id);// Verifica si el id coincide y lo retorna
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-
-    async updateProduct(id, newTitle) {
-        try {
-            const productToUpdate = await this.getProductById(id); // Utiliza getProductById para obtener el producto 
-    
-            if (productToUpdate) { // Verifica si se encontró el producto
-                const products = await this.getProducts(); // Obténgo todos los productos
-                const productIndex = products.findIndex(product => product.id === id); // Encuentra el índice del producto a actualiza
-    
-                if (productIndex !== -1) {// verifica
-                    products[productIndex].title = newTitle; // Actualiza el título del producto
-                    await fs.promises.writeFile(this.path, JSON.stringify(products)); // Escribe el array completo actualizado
-                }
-
-            } else {
-                console.log(`Producto con Id ${id} no encontrado.`);
-            }
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-
-
-    async deleteProduct(id){
-        try {
-            const deleteProduct = await this.getProductById(id); // Utiliza getProductById para obtener el producto específico
-            if(deleteProduct){
-                const products = await this.getProducts(); // Obténgo todos los productos
-                const productIndex = products.findIndex(product => product.id === id); // Busca en este caso el id del producto a eliminar
-
-                if (productIndex !== -1) { // verifica 
-                    products.splice(productIndex, 1); // Elimina el producto del array
-                    await fs.promises.writeFile(this.path, JSON.stringify(products)); // Escribe el array actualizado en el archivo
-                    console.log(`Producto con Id ${id} eliminado.`);
-                } else {
-                    console.log(`Producto con Id ${id} no encontrado.`);
-                }
-            }
-        } catch (error) {
-            console.log(error);
-        }
-
-    }
-
-    getProductoID(){
-        const productoID = Math.floor(Math.random()*1000);// Genera un id ramdon
-        return productoID;//Lo retorna
-    }
-
 }
-
-const manager = new ProductManager("../../users.json");
-
-const producto1 = {
-    title: "Pera",
-    description:"color Yellow",
-    thumbnail:"http://",
-    code:1716,
-    id:"",
-    stock:3000
-};
-
-const producto2 = {
-    
-    title: "Manzana",
-    description:"color red",
-    thumbnail:"http://",
-    code:1711,
-    id:"",
-    stock:3000
-};
-
-const test = async() =>{
-    
-    //await manager.addProducts(producto2) // agregamos un producto previamente creado
-    //await manager.addProducts(producto1)// agregamos un producto previamente creado
-    await manager.deleteProduct(98);  // Reemplaza 78 con el ID del producto que quieres eliminar
-    //await manager.updateProduct(982,"titulo actualizado") //una ves guardado un producto en el archivo
-                                                        // lo podemos modificar llamandolo por id y actualizarlo
-    console.log(await manager.getProducts()) //Llama a los products
-
-}
-test()
-
-// Una ves que cargamos los productos los podemos elimnar y actualizar cumpliendo con lo pedido!
